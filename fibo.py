742b1a3b6bdad17e15cbad46c99d8dc964b272ff
Chang Square <38764292+changsquare@users.noreply.github.com>
2019-09-29 18:28:42 -0700

    my second commit

--- fibo.py
+++ fibo.py
@@ -1,23 +1,41 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Apr 17 13:28:13 2018
+Created on Fri Sep 27 18:32:21 2019
 
-@author: changc1
+@author: chang2
 """
 
-# Fibonacci number generator
-def fib(n):
+'''
+Executing modules as scripts:
+the code in the module will be executed, just as if you imported it, but with 
+the __name__ set to "__main__". That means that by adding this code at the end
+of your module:
+'''
+
+aa = [1, 2, 9]
+bb = [33, 45, 23]
+
+def fib1(n):    # write Fibonacci series up to n
     a, b = 0, 1
-    while b < n:
-        print (b, end=',')
+    while a < n:
+        print(a, end=' ')
         a, b = b, a+b
-        
-def fib2(n):
-    result=[]
-    print(id(result))
+    print('__name__ inside the Fibo module is: {0}'.format(__name__))
+    
+
+def fib2(n):   # return Fibonacci series up to n
+    result = []
     a, b = 0, 1
-    while b < n:
-        result.append(b)
+    while a < n:
+        result.append(a)
         a, b = b, a+b
     return result
-        
+
+
+
+if __name__ == "__main__":
+    import sys
+    fib1(int(sys.argv[1]))
+    print('Name is {0}.'.format(__name__))
+
